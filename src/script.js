7517453d360cad29c0a906058869d7aca0da3bb9
Sky K <skyj.kim7@gmail.com>
2023-02-07 09:07:19 -0800

    bootstrap link working

--- src/script.js
+++ src/script.js
@@ -5,13 +5,13 @@ const teamRender = (team) => {
     <h5 class="card-title">${manager.name}</h5>
     <p class="card-text">
     <ul class="detail-list">
+    <li>Manager</li>
     <li>${manager.id} </li>
     <li>Email: <a href="mailto: ${manager.email}">${manager.email}</a></li>
     <li>Office Number: ${manager.officeNumber}</li>
     </ul>
     
     </p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>`;
 
@@ -19,34 +19,34 @@ const teamRender = (team) => {
 
   const renderIntern = (intern) =>
     `<div class="card" style="width: 18rem;">
-<div class="card-body">
-<h5 class="card-title">${intern.name}</h5>
-<p class="card-text">
-<ul class="detail-list">
-<li>${intern.id} </li>
-<li>Email: <a href="mailto: ${intern.email}">${intern.email}</a></li>
-<li>Office Number: ${intern.school}</li>
-</ul>
+    <div class="card-body">
+    <h5 class="card-title">${intern.name}</h5>
+    <p class="card-text">
+    <ul class="detail-list">
+    <li>Intern</li>
+    <li>${intern.id} </li>
+    <li>Email: <a href="mailto: ${intern.email}">${intern.email}</a></li>
+    <li>Office Number: ${intern.school}</li>
+    </ul>
 
 </p>
-<a href="#" class="btn btn-primary">Go somewhere</a>
 </div>
 </div>`;
 
   //rendering Engineer
   const renderEngineer = (engineer) =>
     `<div class="card" style="width: 18rem;">
-<div class="card-body">
-<h5 class="card-title">${engineer.name}</h5>
-<p class="card-text">
-<ul class="detail-list">
-<li>${engineer.id} </li>
-<li>Email: <a href="mailto: ${engineer.email}">${engineer.email}</a></li>
-<li>Office Number: ${engineer.gitHub}</li>
-</ul>
+    <div class="card-body">
+    <h5 class="card-title">${engineer.name}</h5>
+    <p class="card-text">
+    <ul class="detail-list">
+    <li>Engineer</li>
+    <li>${engineer.id} </li>
+    <li>Email: <a href="mailto: ${engineer.email}">${engineer.email}</a></li>
+    <li>Office Number: ${engineer.gitHub}</li>
+    </ul>
 
 </p>
-<a href="#" class="btn btn-primary">Go somewhere</a>
 </div>
 </div>`;
 
@@ -84,6 +84,14 @@ const mainHTML = (team) =>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+    crossorigin="anonymous"
+    />
+
     <title>Team</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./style.css" />
@@ -100,6 +108,7 @@ const mainHTML = (team) =>
         <div
           class="team-mem-container col-12 d-flex flex-wrap justify-content-around"
         >
+        <br/> <br/>
           ${teamRender(team)}
         </div>
       </div>
